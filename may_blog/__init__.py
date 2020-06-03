c6e0a856d128eeec6b5e75f247f23e6aad5c1977
aa-ag <aaron.aguerrevere@gmail.com>
2020-06-03 16:43:56 -0500

    day3

--- may_blog/__init__.py
+++ may_blog/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from config import Config
 
-# Create flask app variable
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+from flask_mail import Mail, Message
+
+from flask_login import LoginManager
+
+app = Flask(__name__)
 app.config.from_object(Config)
 
-from may_blog import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+mail = Mail(app)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+from may_blog import routes, models
--- may_blog/routes.py
+++ may_blog/routes.py
@@ -1,7 +1,10 @@
-from may_blog import app
-from flask import render_template
+from may_blog import app, db, Message
+from flask import render_template, request, redirect, url_for
+from may_blog.forms import UserInfoForm, PostForm, LoginForm
+from may_blog.models import User, Post, check_password_hash
+from flask_login import login_required, login_user, current_user, logout_user
 
-# Home route
+
 @app.route('/')
 def home():
     customer_name = "Aaron"
@@ -9,7 +12,52 @@ def home():
     item_dict = {1: "Ice Cream", 2:"Bread", 3:"Lemons", 4:"Cereal"}
     return render_template("home.html", customer_name = customer_name, order_number = order_number, item_dict = item_dict)
 
-# Register route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = UserInfoForm()
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        print("\n", username, password, email)
+        
+        user = User(username, email, password)
+        
+        db.session.add(user)
+        
+        db.session.commit()
+        
+        msg = Message(f"Thanks for signingup! {email}", recipients=[email])
+        msg.body = ('Congrats on sigingup! Looking forward to your posts!')
+        msg.html = ('<h1> Welcome to May Blog</h1>' '<p> This will be fun! </p>')
+
+    return render_template('register.html', form = form)
+
+@app.route('/posts', methods=['GET', 'POST'])
+@login_required
+def posts():
+    post = PostForm()
+    if request.method == 'POST' and post.validate():
+        title = post.title.data
+        content = post.content.data
+        print("\n", title, content)
+    return render_template('posts.html', post = post)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        password = form.password.data
+        logged_user = User.query.filter(User.email == email).first()
+        if logged_user and check_password_hash(logged_user.password, password):
+            login_user(logged_user)
+            return redirect(url_for('home'))
+        else:
+            return redirect(url_for('login'))
+    return render_template('login.html', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
